fc2345003338c9f89e717e86ca4d0d29759382ad
Sanjulaarora <sanjulaarora3005@gmail.com>
2024-09-25 14:52:25 +0530

    feed optimized

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import Blog from './Blog';
+import React, { Suspense } from 'react';
+
+const Blog = React.lazy(() => import('./Blog'));
 
 const Feed = ({blogs}) => {
   return (
     <>
       {blogs.map(blog =>(
-        <Blog key={blog.id} blog={blog} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Blog key={blog.id} blog={blog} />
+        </Suspense>
       )) }
     </>
   )
